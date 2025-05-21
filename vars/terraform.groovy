0bc7e050d9c7edc840a33dc6baa91060120d5ae8
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-21 16:32:40 -0500

    roboshop jenkins

---
+++ vars/terraform.groovy
@@ -0,0 +1,31 @@
+def call(){
+    pipeline {
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick environment')
+        }
+        stages {
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+            stage('Terraform action') {
+                steps {
+                    sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
