0d0c91842182d9fa48dcbfcbe3e9bb7987c6f7df
naveenporeddy <naveenkumar9707@gmail.com>
2025-06-20 13:12:48 -0500

    helm

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -31,9 +31,9 @@ def call(){
                     sh 'echo $TAG_NAME > VERSION'
                    // sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
                     //sh 'curl -v -u admin:admin --upload-file ${component}-${TAG_NAME}.zip http://172.31.42.37:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                    sh 'sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 071312222500.dkr.ecr.us-east-1.amazonaws.com'
-                    sh 'sudo docker build -t  071312222500.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-                    sh 'sudo docker push 071312222500.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 071312222500.dkr.ecr.us-east-1.amazonaws.com'
+                    sh 'docker build -t 071312222500.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+                    sh 'docker push 071312222500.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
                 }
             }
 
